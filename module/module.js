a4f624a2f581b3024b2d9ff9fecebfc0b2956d90
Benjamin Fortenberry <benfortenberry@outlook.com>
2020-05-02 15:53:04 -0500

    cleanup

--- module/module.js
+++ module/module.js
@@ -1,16 +1,4 @@
 ﻿var app;
 (function () {
     app = angular.module("u2016", ['ui.bootstrap']);
-
-
-    //app.filter('ageFilter', function () {
-    //    return function (birthday) {
-    //        var birthday = new Date(birthday);
-    //        var today = new Date();
-    //        var age = ((today - birthday) / (31557600000));
-    //        var age = Math.floor(age);
-    //        return age;
-    //    }
-    //});
-
 })();
